b3eb114d69a7a9e519a9ad57a1601ed8b2298971
Guilherme Alacoc <guilhermealacoc@gmail.com>
2020-07-03 23:35:53 -0300

    Atualização e finalização do projeto de estudos.

--- lib/main.dart
+++ lib/main.dart
@@ -4,58 +4,91 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       title: "Contador de Pessoas",
-      home: Stack(
-        children: <Widget>[
-          Image.asset("Images/virus.jpg",
+      home: Home()));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _people = 0;
+  String _infoText = "Pode Entrar";
+  
+  void _changePeople(int delta){
+    setState(() {
+      _people += delta;
+
+      if(_people < 0){
+        _infoText = "Verifique novamente a contagem!!!";
+      } else if(_people <= 10){
+        _infoText = "Pode entrar";
+      } else{
+        _infoText = "Lotado!!!";
+      }
+
+    });
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          "images/virus.jpg",
           fit: BoxFit.cover,
           height: 1000.00,),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                "Pessoas: 0",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Pessoas: $_people",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: FlatButton(
-                      child: Text(
-                        "+1",
-                        style: TextStyle(fontSize: 40.00, color: Colors.white),
-                      ),
-                      onPressed: (){},
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "+1",
+                      style: TextStyle(fontSize: 40.00, color: Colors.white),
                     ),
+                    onPressed: (){
+                      _changePeople(1);
+                    },
                   ),
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: FlatButton(
-                      child: Text(
-                        "-1",
-                        style: TextStyle(fontSize: 40.00, color: Colors.white),
-                      ),
-                      onPressed: (){},
-                    ),
-                  ),
-                ],
-              ),
-              Text(
-                "Pode Entrar!",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30.0,
                 ),
-              )
-            ],
-          )
-        ],
-      )
-    )
-  );
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "-1",
+                      style: TextStyle(fontSize: 40.00, color: Colors.white),
+                    ),
+                    onPressed: (){
+                      _changePeople(-1);
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              _infoText,
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 30.0,
+              ),
+            )
+          ],
+        )
+      ],
+    );
+  }
 }
